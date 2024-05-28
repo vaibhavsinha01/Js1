355b0ac713d6181310943ab620418098e7c0b44e
Vaibhav Sinha <vaibhavrajsinha099@gmail.com>
2024-05-28 20:15:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const textarea=document.querySelector("textarea")
+const button=document.querySelector("button")
+
+const textToSpeech = () =>{
+    const synth=window.speechSynthesis
+    const text=textarea.value
+
+    const utterance=new SpeechSynthesisUtterance(text)
+    synth.speak(utterance)
+}
+
+button.addEventListener("click",textToSpeech);
